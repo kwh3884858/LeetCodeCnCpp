23722e521f9e2d302d64bb7e8d78884a9f28275f
Kong Wei Hang <kwh3884858@gmail.com>
2022-02-15 23:35:27 +0800

    2022-02-15

---
+++ 0142.环形链表II/0142-环形链表II.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow)
+            {
+                slow = head;
+                while(fast != slow)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return fast;
+            }
+        }
+        return nullptr;
+    }
+};

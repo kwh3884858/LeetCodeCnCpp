f4bcc7ed54ccf741c65613652d590c65eaa84c45
Kong Wei Hang <kwh3884858@gmail.com>
2022-02-16 23:10:50 +0800

    2022-02-16

---
+++ 0160.相交链表/0160-相交链表.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* left = headA;
+        ListNode* right = headB;
+        while(left != right)
+        {
+            if(left == nullptr) left = headB;
+            else left = left -> next;
+            if(right == nullptr) right = headA;
+            else right = right -> next;
+        }
+        return left;
+    }
+};
